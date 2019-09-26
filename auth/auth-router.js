7ce10963fa86e03ef0a6ecfff6ce6e562ad729dd
Devin Warrick <dev.warr@gmail.com>
2019-09-26 11:31:12 -0500

    testing

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -10,7 +10,7 @@ router.post('/register', (req, res) => {
     let user = req.body;
     const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
     user.password = hash;
-
+    console.log("In the Register Endpoint, line 13")
     Users.insert(user)
         .then(saved => {
             res.status(201).json(saved);
